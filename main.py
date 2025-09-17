f6462bb905690d6d0c01057b9b1cb9cd3c021334
Joel <jitwaru2@hotmail.com>
2025-09-17 04:18:42 -0400

    refactored to pull HTML safer. Created assets folder

--- main.py
+++ main.py
@@ -9,8 +9,23 @@ app = QApplication(sys.argv)
 # Create a browser window
 view = QWebEngineView()
 
+def resource_path(relative_path):
+    # checks if the script is running from a PyInstaller executable.
+    if getattr(sys, 'frozen', False):
+        # PyInstaller puts files in a temporary folder when frozen
+        # sys._MEIPASS → the temporary folder where PyInstaller extracts HTML, CSS, JS, etc.
+        base_path = sys._MEIPASS
+    else:
+        # Running normally in Python
+        # used when running normally, so it still works outside of an exe.
+        base_path = os.path.dirname(__file__)
+    return os.path.join(base_path, relative_path)
+
+
 # Load the local HTML file
-html_file = os.path.join(os.path.dirname(__file__), "index.html")
+# html_file = os.path.join(os.path.dirname(__file__), "index.html")
+html_file = resource_path("index.html")
+
 view.load(QUrl.fromLocalFile(html_file))
 
 # Show the window
